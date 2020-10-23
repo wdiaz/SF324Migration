9acb373aff19c86de95bed4f6f1f8e50566b2462
Walter Diaz <wdiaz@cs.columbia.edu>
2020-10-23 13:15:37 -0400

    removed direct access to the container

--- src/AppBundle/Controller/GenusController.php
+++ src/AppBundle/Controller/GenusController.php
@@ -6,6 +6,7 @@ use AppBundle\Entity\Genus;
 use AppBundle\Entity\GenusNote;
 use AppBundle\Entity\GenusScientist;
 use AppBundle\Service\MarkdownTransformer;
+use Psr\Log\LoggerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -75,14 +76,13 @@ class GenusController extends Controller
     /**
      * @Route("/genus/{slug}", name="genus_show")
      */
-    public function showAction(Genus $genus, MarkdownTransformer $markdownTransformer)
+    public function showAction(Genus $genus, MarkdownTransformer $markdownTransformer, LoggerInterface $logger)
     {
         $em = $this->getDoctrine()->getManager();
 
         $funFact = $markdownTransformer->parse($genus->getFunFact());
 
-        $this->get('logger')
-            ->info('Showing genus: '.$genus->getName());
+        $logger->info('Showing genus: '.$genus->getName());
 
         $recentNotes = $em->getRepository('AppBundle:GenusNote')
             ->findAllRecentNotesForGenus($genus);
